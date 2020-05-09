e4b9ca6a317d787d39a580ecb168a0f245e3657d
Sankalok Sen <62195537+sankalok@users.noreply.github.com>
2020-05-09 19:47:55 +0530

    Final Edits
    Final Edits

--- highscores.cpp
+++ highscores.cpp
@@ -6,32 +6,54 @@
 #include "highscores.h"
 using namespace std;
 //printing the top 10 highscores
-void printHighScores(string userName, long long userScore)	//Player's userName and userScore
+void printHighScores()	//Player's userName and userScore
 {
 	ifstream fin;
+	ifstream fin2;
 	fin.open("highscores.txt");	//opening highscores.txt
+	fin2.open("highscores.txt"); //opening highscores.txt
+	string name;	//name at each counter
+	long long score;	//score at each counter
+	long long count=0;
+	while(fin2>>name>>score)
+	{
+		count+=1;
+	}
 	if(fin.fail())
 	{
 		cout<<"Error in opening File."<<endl;
 	}
+	if(fin2.fail())
+	{
+		cout<<"Error in opening File."<<endl;
+	}
 	int i=1;	//counter
-	string name;	//name at each counter
-	long long score;	//score at each counter
 	cout<<setfill('*')<<setw(47)<<""<<endl;
 	cout<<"******************HIGH SCORES******************"<<endl;
 	cout<<setfill('*')<<setw(47)<<""<<endl;
 	cout<<"POS"<<setfill(' ')<<setw(24)<<"NAME"<<setfill(' ')<<setw(20)<<"SCORE"<<endl;
-	while(i<=10)	//while loop to print the formatted highscores for the top 10
+	if(count>=10)
 	{
-		fin>>name>>score;
-    	cout<<setfill('0')<<setw(3)<<i<<setfill(' ')<<setw(24)<<name<<setfill(' ')<<setw(20)<<score<<endl;
-		i+=1;
+		while(i<=10)	//while loop to print the formatted highscores for the top 10
+		{
+			fin>>name>>score;
+    		cout<<setfill('0')<<setw(3)<<i<<setfill(' ')<<setw(24)<<name<<setfill(' ')<<setw(20)<<score<<endl;
+			i+=1;
+		}
 	}
-	cout<<setfill('*')<<setw(47)<<""<<endl;
-	cout<<"USERNAME: "<<userName<<endl;	//prints the userName and userScore
-	cout<<"USER SCORE: "<<userScore<<endl;
-	cout<<setfill('*')<<setw(47)<<""<<endl;
-	cout<<"*****YOU HAVE REACHED THE END OF THE HEIST*****"<<endl;
+	else
+	{
+		while(i<=count)	//while loop to print the formatted highscores for the top 10
+		{
+			fin>>name>>score;
+    		cout<<setfill('0')<<setw(3)<<i<<setfill(' ')<<setw(24)<<name<<setfill(' ')<<setw(20)<<score<<endl;
+			i+=1;
+		}
+		for(i=count; i<=10; i++)
+		{
+			cout<<setfill('0')<<setw(3)<<i<<setfill(' ')<<setw(24)<<"---"<<setfill(' ')<<setw(20)<<0<<endl;
+		}
+	}
 	cout<<setfill('*')<<setw(47)<<""<<endl;
 	fin.close();
 	//end of the game
